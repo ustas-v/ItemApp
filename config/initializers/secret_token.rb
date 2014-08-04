3df42588ef5ee6f9670c0053d209c95cb3631cfa
Vladimir Bogutskiy <ustas.v@gmail.com>
2014-08-04 09:57:48 +0400

    Init repo ItemApp.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItemApp::Application.config.secret_token = 'c0de9402c45019cb63412e0e0febea8fe2097301747079f42ab8ade4b2e120a5b488b85dd16a78e468bc1322227bb4c83eafc6529488ae77ca4756706f2bb2a8'
